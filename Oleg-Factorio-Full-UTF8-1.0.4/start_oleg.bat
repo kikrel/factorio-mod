ffac025c0b1c15ba8403cba69c8e7690707da7cc
kikrel <kikrel@outlook.com>
2026-08-17 14:00:08 +1000

    Delete Oleg-Factorio-Full-UTF8-1.0.4 directory

--- Oleg-Factorio-Full-UTF8-1.0.4/start_oleg.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001 >nul
-title Oleg Bridge for Factorio
-powershell -NoProfile -ExecutionPolicy Bypass -Command "[Console]::OutputEncoding = [System.Text.Encoding]::UTF8; [Console]::InputEncoding = [System.Text.Encoding]::UTF8; & '%~dp0bridge.ps1'"
-pause
